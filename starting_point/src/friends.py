2b53eae7c141f3128191655816c651de05a1246c
lushpurr <lushpurr@gmail.com>
2020-10-15 12:04:16 +0100

    Second commit

--- starting_point/src/friends.py
+++ starting_point/src/friends.py
@@ -1,5 +1,16 @@
+import pdb
+
 def get_name(person):
     return person["name"]
 
 def get_favourite_tv_show(person):
     return (person["favourites"]["tv_show"])
+
+def likes_to_eat(person, food):
+    #pdb.set_trace()
+    for snack in person["favourites"]["snacks"]:
+        if snack == food:
+            return True
+        
+    return False
+
