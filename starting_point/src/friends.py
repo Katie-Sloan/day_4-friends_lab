398668d5e14868fe6bb013db03d6acd9ad473c0c
lushpurr <lushpurr@gmail.com>
2020-10-15 14:11:38 +0100

    Third Commit

--- starting_point/src/friends.py
+++ starting_point/src/friends.py
@@ -14,3 +14,13 @@ def likes_to_eat(person, food):
         
     return False
 
+
+ # 4. For a given person, add a new name to their list of friends
+  # (e.g. the function add_friend(self.person2, "Scrappy-Doo") should add Scrappy-Doo to the friends.)
+  # (hint: This function should not return anything. 
+  # After the function call, check for the length of the friends array to test it!)
+  
+def add_friend(person, friend):
+    person["friends"] = person["friends"].append("friend")
+
+    return len(person["friends"])
